c47a098c12e1a34a976c41ab1e9a6224b59c9e2e
Tsyriulnikov <max_djvu@mail.ru>
2022-05-17 16:28:09 +0300

    storybook v0.3

--- src/stories/AppWithRedux.stories.tsx
+++ src/stories/AppWithRedux.stories.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import { AppWithRedux } from '../AppWithRedux';
-import {action} from "@storybook/addon-actions";
+import  AppWithRedux  from '../AppWithRedux';
+import {Provider} from "react-redux";
+import {store} from "../state/store";
+import {ReduxStoreProviderDecorator} from "./ReduxStoreProviderDecorator";
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
     title: 'Todolist/AppWithRedux',
     component: AppWithRedux,
+    decorators:[ReduxStoreProviderDecorator]
+
     } as ComponentMeta<typeof AppWithRedux>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-// const Template: ComponentStory<typeof AppWithRedux> = (args) => <AppWithRedux {...args} />;
 
 
 export const AppWithReduxExample = ()=>{
-    return <AppWithRedux />
-};
+   return (
+        <AppWithRedux />
+   )};
 
 
 
