143ec3c0a77162597c1fa76c69b9d5503a0fae6d
Tsyriulnikov <max_djvu@mail.ru>
2022-05-17 16:32:35 +0300

    storybook v0.3

--- src/App.tsx
+++ src/App.tsx
@@ -178,3 +178,10 @@ function App() {
 }
 
 export default App;
+import React from "react";
+import {Provider} from "react-redux";
+import {store} from "../state/store";
+
+export const ReduxStoreProviderDecorator =(story:any)=>{
+    return <Provider store={store}>{story()}</Provider>
+}
