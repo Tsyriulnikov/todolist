dca0fe2b3d08ab801f8ce7813de00d88cc71976e
Tsyriulnikov <max_djvu@mail.ru>
2022-06-13 21:36:06 +0300

    lesson 16 v. 0.2

--- src/api/todolists-api.ts
+++ src/api/todolists-api.ts
@@ -4,8 +4,7 @@ const instance = axios.create({
     baseURL: 'https://social-network.samuraijs.com/api/1.1/',
     withCredentials: true,
     headers: {
-        'API-KEY': '8f2534e2-22a4-4052-894e-a66c04807482'
-
+        'API-KEY': 'fe698d58-4d10-4f41-a217-bc0f632c98e5'
     }
 })
 
